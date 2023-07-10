f19c532bc48db1181b61de5a113b06eaaff55b96
Ayush Pandit <ayushpanditmoto@gmail.com>
2023-07-10 20:07:03 +0530

    feat 1 question done

--- 19 Two Sum.cpp
+++ 19 Two Sum.cpp
@@ -1,7 +1,28 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        // vector<int> ans;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i+1;j<n;j++){
+        //         if(nums[i]+nums[j]==target){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //             return ans;   
+        //         }
+        //     }
+        // }
+        // return {};
+        map<int,int> mp;
         
+        for(int i=0;i<n;i++){
+            int more=target-nums[i];
+            if(mp.find(more)!=mp.end()){
+                return {mp[more],i};
+            }
+            mp[nums[i]]=i;
+        }
         
+        return {};
     }
 };
