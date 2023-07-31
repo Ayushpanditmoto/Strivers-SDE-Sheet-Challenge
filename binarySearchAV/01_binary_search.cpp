0bf020db1e0d6181c07c61b70ac9ba6f19062bbd
ayush pandit <ayushpanditmoto@gmail.com>
2023-07-31 19:37:23 +0530

    revised: binary search part 2

--- binarySearchAV/01_binary_search.cpp
+++ binarySearchAV/01_binary_search.cpp
@@ -2,12 +2,12 @@ void solution(vector<int> &nums,int target){
    int n=nums.size();
     int low=0,high=n-1;
     while(low<=high){
-        int mid = low + (high-low)/2;
+        int mid = low + (high-low)/2; // it prevent integer overflow 10^9 + 10^9
         if(nums[mid]==target){
             cout<<mid;
             return;
         }
-        else if(nums[mid]>target){
+        else if(target<nums[mid]){
             high = mid-1;
         }
         else{
@@ -15,3 +15,25 @@ void solution(vector<int> &nums,int target){
         }
     }
 }
+
+//2 . Reverse Sorted Array 
+// then just change the condition of binary search
+// if(nums[mid]==target){
+//             cout<<mid;
+//             return;
+//         }
+//         else if(target<nums[mid]){
+//             low = mid+1;
+//         }
+//         else{
+//             high = mid-1;
+//         }
+
+
+// 3 Order of the array is not known
+// then just change the condition of binary search
+//  then check arr[0]<arr[1] or arr[0]>arr[1]
+// to check the order of the array
+
+
+
